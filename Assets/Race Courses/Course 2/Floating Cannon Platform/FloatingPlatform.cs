227863aa4f9ce62dcb0e98d22635018132ac9940
Matt Graber <grabermtw@gmail.com>
2020-12-05 12:16:39 -0500

    Made the floating platforms only attack you if you're wearing a jetpack

--- Assets/Race Courses/Course 2/Floating Cannon Platform/FloatingPlatform.cs
+++ Assets/Race Courses/Course 2/Floating Cannon Platform/FloatingPlatform.cs
@@ -42,38 +42,44 @@ public class FloatingPlatform : MonoBehaviour
     private IEnumerator CannonControl()
     {
         // determine who to aim at
+        // it'll be the closest racer who is also jetpacking
         float minDist = Mathf.Infinity;
+        target = null;
         Rigidbody targetRb = null;
         for (int i = 0; i < racers.Length; i++)
         {
             float dist = Vector3.Distance(cannon.position, racers[i].transform.position);
-            if (dist < minDist)
+            if (dist < minDist && racers[i].movementMode == Movement.Mode.Jetpacking)
             {
                 minDist = dist;
                 target = racers[i].GetHips();
                 targetRb = racers[i].GetComponent<Rigidbody>();
             }
         }
-        // Determine how long to aim for before firing the cannon
-        float aimTime = Random.Range(1, 6);
-        float currentAimTime = 0;
-        while(currentAimTime < aimTime)
+        if (target != null)
         {
-            // Estimate the target's future position by the time the laser reaches them based on their velocity and the laser's speed
-            Vector3 targetPosition = target.position + targetRb.velocity * Vector3.Distance(target.position, cannon.position) / laserSpeed;
-            // Calculate where to aim
-            Quaternion direction = Quaternion.Slerp(cannon.rotation, Quaternion.LookRotation(targetPosition - cannon.position), Time.deltaTime * aimSpeed);
-            // Clamp the rotation so that the barrel doesn't clip through the platform
-            if (direction.eulerAngles.x < 240 || direction.eulerAngles.x > 325)
+            // Determine how long to aim for before firing the cannon
+            float aimTime = Random.Range(1, 6);
+            float currentAimTime = 0;
+            while (currentAimTime < aimTime)
             {
-                cannon.rotation = direction;
+                // Estimate the target's future position by the time the laser reaches them based on their velocity and the laser's speed
+                Vector3 targetPosition = target.position + targetRb.velocity * Vector3.Distance(target.position, cannon.position) / laserSpeed;
+                // Calculate where to aim
+                Quaternion direction = Quaternion.Slerp(cannon.rotation, Quaternion.LookRotation(targetPosition - cannon.position), Time.deltaTime * aimSpeed);
+                // Clamp the rotation so that the barrel doesn't clip through the platform
+                if (direction.eulerAngles.x < 240 || direction.eulerAngles.x > 325)
+                {
+                    cannon.rotation = direction;
+                }
+                currentAimTime += Time.deltaTime;
+                yield return null;
             }
-            currentAimTime += Time.deltaTime;
-            yield return null;
+            // Fire the cannon
+            GameObject laser = Instantiate(projectile, 2 * cannon.forward + cannon.position, cannon.rotation);
+            laser.GetComponent<LaserBolt>().SetSpeed(laserSpeed);
         }
-        // Fire the cannon
-        GameObject laser = Instantiate(projectile, 2 * cannon.forward + cannon.position, cannon.rotation);
-        laser.GetComponent<LaserBolt>().SetSpeed(laserSpeed);
+        yield return null;
         StartCoroutine(CannonControl());
     }
 
